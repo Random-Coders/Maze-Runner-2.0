7ed7863b67d4060f08b67800ab44b1a0d19f2f26
Rafael Cenzano <savagecoder77@gmail.com>
2018-11-17 16:41:59 -0800

    add tiles

--- game.py
+++ game.py
@@ -2,6 +2,24 @@ import pygame
 
 from pygame.locals import *
 
+DIRT = 0
+GRASS = 1
+WATER = 2
+LEAVE = 3
+
+BROWN =
+GREEN =
+LIGHTGREEN =
+BLUE = 
+
+colors = {
+            DIRT  : BROWN,
+            GRASS : GREEN
+            WATER : BLUE
+            LEAVE : LIGHTGREEN
+        }
+
+
 pygame.init()
 
 display_surface = pygame.display.set_mode((1000,1000))
