e23900fac4eab07fb02e0cf3f3f2d8ef63021a64
Rafael Cenzano <savagecoder77@gmail.com>
2018-11-18 08:35:58 -0800

    push

--- camera.py
+++ camera.py
@@ -4,20 +4,20 @@ from settings import *
 
 class Camera(pygame.Rect):
     def __init__(self, targetRect, windowWidth, windowHeight):
-        super(Camera,self).__init__(targetRect.rect.x-(windowWidth/2),
-                                    targetRect.rect.y-(windowHeight/2),
-                                    windowWidth, windowHeight)
+        #super(Camera,self).__init__(targetRect.rect.x-(windowWidth/2),
+        #                            targetRect.rect.y-(windowHeight/2),
+    #                                windowWidth, windowHeight)
         self.cameraxposition = 80
         self.camerayposition = 60
 
     def update(self, rect, level):
         if rect.x - rect.x > self.cameraxposition:
             self.left = rect.x + self.cameraxposition - self.width/2
-        elif rect.x - self.rect.x > self.cameraxposition:
+        elif rect.x - rect.x > self.cameraxposition:
             self.left = rect.x - self.cameraxposition - self.width/2
         if rect.y - rect.y > self.camerayposition:
             self.top = rect.y + self.camerayposition - self.height/2
-        elif rect.y - self.rect.y > self.camerayposition:
+        elif rect.y - rect.y > self.camerayposition:
             self.top = rect.y - self.camerayposition - self.height/2
 
         # This keeps the camera within the boundaries of the level
